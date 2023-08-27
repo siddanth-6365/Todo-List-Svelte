b3ee79a4858ca5beef7d7f88fce50c09415dded4
Sidddanth Katipally <siddanth6365@gmail.com>
2023-08-27 23:26:45 +0530

    removed warnings

--- src/routes/todo/DislplayLists.svelte
+++ src/routes/todo/DislplayLists.svelte
@@ -1,35 +1,46 @@
 <script lang="ts">
   import { listStore, CurrentListStore } from "./stores/todo";
+  import { readable, writable } from 'svelte/store';
 
-  
+  interface ListItem {
+    id: number;
+    title: string;
+    desc: string;
+  }
 
-  let listArr = [];
-
+  let listArr: ListItem[] = [];
   listStore.subscribe((value) => {
     listArr = value;
   });
 
+  const typedListStore = readable<ListItem[]>([], (set) => {
+    listStore.subscribe((value) => {
+      set(value);
+    });
+  });
+
+  let id: number = 0;
+
   $: {
-    listArr = $listStore;
+    id = $CurrentListStore;
   }
 
-  const DeleteList = (itemIdToDelete) => {
+  const DeleteList = (itemIdToDelete: number) => {
     listStore.update((arr) => {
       return arr.filter((existingItem) => existingItem.id !== itemIdToDelete);
     });
-    let idd=1;
+    const idd: number = 1;
     CurrentListStore.update(() => idd);
-    
   };
 
-  const SetCurrentList = (itemId) => {
+  const SetCurrentList = (itemId: number) => {
     CurrentListStore.update(() => itemId);
   };
 </script>
 
 <div class="flex flex-col mt-4">
   {#each listArr as item (item.id)}
-    <div class="flex mb-2" >
+    <div class="flex mb-2">
       <div
         class="flex w-96 flex-col border border-white rounded p-2 hover:bg-gray-800 cursor-pointer"
         on:click={() => {
@@ -37,10 +48,7 @@
         }}
         class:bg-gray-800={item.id === $CurrentListStore}
       >
-        <span
-          class="text-2xl capitalize font-semibold "
-          >{item.title}</span
-        >
+        <span class="text-2xl capitalize font-semibold">{item.title}</span>
         <p class="text-sm text-gray-400">
           {#if item.desc.length < 10}
             {item.desc}
@@ -49,16 +57,8 @@
           {/if}
         </p>
       </div>
-      <div
-        class="mt-4 ml-2 cursor-pointer"
-        on:click={() => DeleteList(item.id)}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="34"
-          height="34"
-          viewBox="0 0 24 24"
-        >
+      <div class="mt-4 ml-2 cursor-pointer" on:click={() => DeleteList(item.id)}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24">
           <path
             fill="currentColor"
             d="M7 21q-.825 0-1.413-.588T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.588 1.413T17 21H7Zm2-4h2V8H9v9Zm4 0h2V8h-2v9Z"
@@ -68,3 +68,4 @@
     </div>
   {/each}
 </div>
+
--- src/routes/todo/EditList.svelte
+++ src/routes/todo/EditList.svelte
@@ -1,29 +1,38 @@
-<script>
+<script lang="ts">
   import { listStore, CurrentListStore } from "./stores/todo";
+  import { readable, writable } from 'svelte/store';
+  interface ListItem {
+    id: number;
+    title: string;
+    desc: string;
+  }
 
-  let id;
-  let selectedItem= null;
+  let id:number;
+    let selectedItem: ListItem | null = null;
 
-  // Subscribe to CurrentListStore and update id
-  CurrentListStore.subscribe((value) => {
-    id = value;
+
+  const typedListStore = readable<ListItem[]>([], (set) => {
+    listStore.subscribe((value) => {
+      set(value);
+    });
   });
 
   $: {
     id = $CurrentListStore;
-    selectedItem = $listStore.find((item) => item.id === +id);
+    selectedItem = $typedListStore.find((item) => item.id === +id) || null;
   }
 
-  function handleTitleChange(event) {
+
+  function handleTitleChange(event: Event) {
     if (selectedItem) {
-      selectedItem.title = event.target.value;
+      selectedItem.title = (event.target as HTMLInputElement).value;
       listStore.update((arr) => [...arr]);
     }
   }
 
-  function handleDescChange(event) {
+  function handleDescChange(event: Event) {
     if (selectedItem) {
-      selectedItem.desc = event.target.value;
+      selectedItem.desc = (event.target as HTMLTextAreaElement).value;
       listStore.update((arr) => [...arr]);
     }
   }
@@ -31,20 +40,22 @@
 
 <div class="w-full max-w-md mx-auto p-4">
   {#if selectedItem}
-    <label class="block mb-2 font-semibold">Title:</label>
+    <label class="block mb-2 font-semibold">Title:
     <input
       type="text"
       class="w-full border p-2 rounded"
       bind:value={selectedItem.title}
       on:input={handleTitleChange}
     />
+  </label>
 
-    <label class="block mt-4 mb-2 font-semibold">Description:</label>
+    <label class="block mt-4 mb-2 font-semibold">Description:
     <textarea
       class="w-full border p-2 rounded"
       bind:value={selectedItem.desc}
       on:input={handleDescChange}
     />
+  </label>
   {:else}
     <p class="text-red-500">Item not found.</p>
   {/if}
